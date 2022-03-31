7420bc646d5b417e7a4f65eea0d8d20d5ddb15a7
irzhdi <aliffffffff@gmail.com>
2022-03-31 10:46:21 +0700

    edit utils

--- src/main/java/com/juaracoding/cucumber4/utlis/Utils.java
+++ src/main/java/com/juaracoding/cucumber4/utlis/Utils.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
